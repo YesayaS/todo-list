69d3aa3547ed19f02bdaf65e612ffea25a386c40
YesayaS <43375421+YesayaS@users.noreply.github.com>
2023-09-12 17:16:07 +0700

    create build

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,6 @@ module.exports = {
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        // include: path.resolve(__dirname, "src/assets"),
         type: "asset/resource",
       },
       {
